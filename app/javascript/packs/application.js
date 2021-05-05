160bee99bc6df37888786050296c26767c7c7b85
SJMQ <sjmccue@hotmail.co.uk>
2021-05-05 18:21:26 +0100

    colspace and tidy up index view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require tether
+//=require bootstrap
+import bootstrap
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
