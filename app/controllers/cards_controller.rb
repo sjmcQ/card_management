aa7cf4ccde5a66712353e20c705b5eea62756177
SJMQ <sjmccue@hotmail.co.uk>
2021-05-08 08:31:22 +0100

    add new card - tidy up CSS

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,4 +7,19 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    
+  end
+
+  def create
+    
+    @card = Card.new(params.require(:card).permit(:card_name, :card_number, :card_balance, 
+      :card_balance_date, :card_min_payment, :card_apr, :card_priority, :card_type, :card_provider, :card_expiry_date
+     )
+    )
+    @card.save
+    redirect_to card_path(@card)
+  end
+
 end
